f353a131577e911f3108c6a161e626e9011c1234
Phongsathon Wandee <phongsathon@sirisoft.co.th>
2024-06-13 12:13:42 +0700

    [implement][login]: add jwt token middleware and login

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
 	"strconv"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	jwtware "github.com/gofiber/jwt/v2"
+	"github.com/gofiber/template/html/v2"
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/joho/godotenv"
 )
 
 type Movie struct {
@@ -13,19 +21,45 @@ type Movie struct {
 	Year     int    `json:"year"`
 }
 
+type User struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
 var movies []Movie
 
 func main() {
-	app := fiber.New()
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("load .env failed")
+	}
+
+	engine := html.New("./views", ".html")
+
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
 
 	movies = append(movies, Movie{ID: 1, Title: "Inception", Director: "Christopher Nolan", Year: 2010})
 	movies = append(movies, Movie{ID: 2, Title: "BlacKkKlansman", Director: "Spike Lee", Year: 2018})
+
+	app.Post("/login", login)
+	app.Get("/config", getEnv)
+
+	app.Use(jwtware.New(jwtware.Config{
+		SigningKey: []byte(os.Getenv("JWT_SECRET")),
+	}))
+
+	app.Use(logging)
+
+	app.Use(checkMiddleware)
 
 	app.Get("/movies", getMovies)
 	app.Get("/movies/:id", getMovie)
 	app.Post("/movies", createMovie)
 	app.Put("/movies/:id", updateMovie)
 	app.Delete("/movies/:id", deleteMovie)
+	app.Post("/poster", uploadFile)
+	app.Get("/test-html", testHTML)
 
 	app.Listen(":8080")
 }
@@ -100,4 +134,93 @@ func deleteMovie(c *fiber.Ctx) error {
 	}
 
 	return c.SendStatus(fiber.StatusNotFound)
+}
+
+func uploadFile(c *fiber.Ctx) error {
+	file, err := c.FormFile("poster")
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+
+	err = c.SaveFile(file, "./uploads/"+file.Filename)
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+	}
+
+	return c.SendString("File Upload Complete!!")
+}
+
+func testHTML(c *fiber.Ctx) error {
+	return c.Render("index", fiber.Map{
+		"Title":  "Hello, World!!",
+		"Auther": "Book",
+	})
+}
+
+func getEnv(c *fiber.Ctx) error {
+	return c.JSON(fiber.Map{
+		"JWT_SECRET": os.Getenv("JWT_SECRET"),
+	})
+}
+
+func logging(c *fiber.Ctx) error {
+	start := time.Now()
+
+	fmt.Printf("URL = %s, METHOD = %s, TIME = %s\n", c.OriginalURL(), c.Method(), start)
+
+	return c.Next()
+}
+
+var member = User{
+	Email:    "hello@example.com",
+	Password: "P@ssw0rd",
+}
+
+func login(c *fiber.Ctx) error {
+	user := new(User)
+
+	if err := c.BodyParser(user); err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+
+	if user.Email != member.Email || user.Password != member.Password {
+		return fiber.ErrUnauthorized
+	}
+
+	// Using HS256 signing method
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"email": user.Email,
+		"role":  "admin",
+		"exp":   time.Now().Add(time.Hour * 72).Unix(),
+	})
+
+	// Fetching JWT secret from environment variable
+	secret := os.Getenv("JWT_SECRET")
+	if secret == "" {
+		return c.Status(fiber.StatusInternalServerError).SendString("JWT secret not set")
+	}
+
+	t, err := token.SignedString([]byte(secret))
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+	}
+
+	return c.JSON(fiber.Map{
+		"message": "Login Successfully !!",
+		"token":   t,
+		"err":     err,
+	})
+}
+
+func checkMiddleware(c *fiber.Ctx) error {
+	user := c.Locals("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+
+	if claims["role"] != "member" {
+		return fiber.ErrUnauthorized
+	}
+
+	return c.Next()
 }
